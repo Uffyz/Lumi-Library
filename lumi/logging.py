a3f3c07ad0f03b6789304e61a786e16f7066c4fe
Uffyz <atrofimov619@gmail.com>
2025-02-17 15:17:54 +0400

    k

--- lumi/logging.py
+++ lumi/logging.py
@@ -26,16 +26,18 @@ def new_embed(inter, etype):
 
 async def log(inter, etype='none'):
     log_chnl: TextChannel = inter.client.get_channel(1268559604116226051)
-    msg = await inter.original_response()
     embed = new_embed(inter, etype)
-    data = inter.data.options
-    if data != []:
-        embed.description += "\n\n**Аргументы**\n"
-        for i in data:
-            name = i.name.replace('_', '\\_')
-            embed.description += f"{name}: {i.value}\n"
-    view = View()
-    view.add_item(Button(style=ButtonStyle.url, label="К сообщению", url=msg.jump_url))
+    msg = await inter.original_response()
+    view = None
+    if msg:
+        data = inter.data.options
+        if data != []:
+            embed.description += "\n\n**Аргументы**\n"
+            for i in data:
+                name = i.name.replace('_', '\\_')
+                embed.description += f"{name}: {i.value}\n"
+        view = View()
+        view.add_item(Button(style=ButtonStyle.url, label="К сообщению", url=msg.jump_url))
     await log_chnl.send(embed=embed, view=view)
 
 
