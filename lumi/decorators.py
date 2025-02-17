ffebc17602f5e0d4cba7dae2d8b75839be219f37
Uffyz <atrofimov619@gmail.com>
2025-02-17 15:14:08 +0400

    k

--- lumi/decorators.py
+++ lumi/decorators.py
@@ -15,9 +15,12 @@ def has_perm(admin=None, analyst=None):
         async def wrapper(inter: disnake.ApplicationCommandInteraction, *args, **kwargs):
             self = kwargs.pop('self', None)
             guild = inter.bot.get_guild(guilds_ids[0])
+            logscmd = ["выдать", "души", "контракты", "отобрать", "пополнить", "причуда", "char_delete", "char_give", "idchange"]
             if not inter.guild:
                 return None
             if inter.guild.id not in guilds_ids:
+                if inter.data.name.lower() in logscmd:
+                    await log(inter, 'guild')
                 embed = embed_constructor(title='Ошибка доступа', color='ffffff', image=guild.banner.url)
                 embed.description += '**Эта команда доступна только на нашем [официальном сервере](https://discord.gg/sJhvsyQETu)\n\nЗдесь вы можете погрузиться в увлекательный мир RP, создать уникального персонажа, взаимодействовать с другими участниками и развивать свою историю. У нас дружелюбное сообщество, интересные события и захватывающие игровые сценарии.\n\nПрисоединяйтесь и станьте частью этого приключения!**'
                 return await inter.response.send_message(embed=embed)
@@ -30,15 +33,14 @@ def has_perm(admin=None, analyst=None):
                     if analyst_role in inter.author.roles:
                         has_access = True
                 if has_access or inter.author.guild_permissions.administrator:
-                    logscmd = ["выдать", "души", "контракты", "отобрать", "пополнить", "причуда", "char_delete",
-                               "char_give",
-                               "idchange"]
                     if inter.data.name.lower() in logscmd:
                         await log(inter)
                     if self:
                         return await func(self, inter, *args, **kwargs)
                     return await func(inter, *args, **kwargs)
                 else:
+                    if inter.data.name.lower() in logscmd:
+                        await log(inter, 'perm')
                     return await error(inter, "У вас нет прав для использования этой команды.")
             if self:
                 return await func(self, inter, *args, **kwargs)
--- lumi/logging.py
+++ lumi/logging.py
@@ -1,18 +1,33 @@
-from disnake import TextChannel, Embed, ButtonStyle
+import disnake
+from disnake import TextChannel, ButtonStyle
 from disnake.ui import View, Button
 from datetime import datetime
+from .embed_constructor import embed_constructor
 
 
-async def log(inter):
+def new_embed(inter, etype):
+    embed = embed_constructor(title='Успешное выполнение',
+                              description=
+                              f'**Сервер** - {inter.guild.name}\n'
+                              f'**Команда** - {inter.data.name}\n'
+                              f'**Выполнил** - {inter.author.mention} (id = {inter.author.id})\n',
+                              color=disnake.Colour.brand_green(),
+                              footer=datetime.now().strftime('%d.%m.%Y %H:%M'))
+    if etype == 'guild':
+        embed.description += '**Команда не была выполнена, так как доступна только на основном сервере**\n'
+        embed.colour = int('ff0000', 16)
+    elif etype == 'perm':
+        embed.description += '**Команда не была выполнена, так как у пользователя недостаточно прав**\n'
+        embed.colour = int('ff0000', 16)
+    if inter.client.application_id != 1260266457405853779:
+        embed.description += '**Команда была выполнена на тестовом боте**'
+        embed.colour = int('00bfff', 16)
+    return embed
+
+async def log(inter, etype='none'):
     log_chnl: TextChannel = inter.client.get_channel(1268559604116226051)
     msg = await inter.original_response()
-    embed = Embed(
-        title="Новое действие",
-        description=f"**Сервер** - {inter.guild.name}\n"
-                    f"**Команда** - {inter.data.name}\n"
-                    f"**Выполнил** - {inter.author.mention} (id = {inter.author.id})"
-    )
-    embed.set_footer(text=datetime.now().strftime("%d.%m.%Y %H:%M"))
+    embed = new_embed(inter, etype)
     data = inter.data.options
     if data != []:
         embed.description += "\n\n**Аргументы**\n"
