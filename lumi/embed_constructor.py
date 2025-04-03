9b1e31976b6f6a39576afc79b8a945ae39d51c74
Uffyz <atrofimov619@gmail.com>
2025-04-03 14:33:53 +0400

    kek

--- lumi/embed_constructor.py
+++ lumi/embed_constructor.py
@@ -1,11 +1,15 @@
 from disnake import Embed, Colour
 
+import requests
+# a = requests.get('https://discord.com/api/v9/invites/lumirp?with_counts=true&with_expiration=true&with_permissions=true')
+# print(a.json()['guild']['icon'])
+
 def embed_constructor(title='', description='', color=None, thumbnail=None, footer=None, image=None) -> Embed:
     embed = Embed(
         title=title,
         description=description
     )
-    url = 'https://cdn.discordapp.com/icons/1119281693262622720/e50b732ea840bfc2b15875da678d0083.webp?size=96'
+    url = 'https://cdn.discordapp.com/icons/1119281693262622720/45c0d814dc80ef7d3afd6a16cbf2145b.webp?size=96'
     if color:
         if isinstance(color, str):
             embed.colour = int(color, 16)
